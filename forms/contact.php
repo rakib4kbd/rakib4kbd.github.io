f9c428915d6d802bea4922a1679b7909b1538089
Rakib Khan <rakib4kbd@gmail.com>
2020-11-28 13:20:46 +0600

    removed smtp creds

--- forms/contact.php
+++ forms/contact.php
@@ -24,12 +24,14 @@
   $contact->subject = $_POST['subject'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+  /*
   $contact->smtp = array(
-    'host' => 'smtp.sendgrid.net',
-    'username' => 'apikey',
-    'password' => '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',
-    'port' => '587'
+    'host' => '',
+    'username' => '',
+    'password' => '',
+    'port' => ''
   );
+  */
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
